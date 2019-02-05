775880606bb311389d467b9cb6466753d3b65f9d
Ivan Popov <ipopov@caesar.team>
2019-02-05 10:48:23 +0300

    [share] feature/CAES-229: added forgotten migration

---
+++ src/Migrations/2019/02/Version20190204143949.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20190204143949 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE fos_user ADD require_master_refresh BOOLEAN DEFAULT \'false\' NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE fos_user DROP require_master_refresh');
+    }
+}
