f563569aaf6f29ab8c0d66ad96c5e9ddb46d37aa
mg-4xxi <mg@4xxi.com>
2019-02-14 12:05:40 +0300

    [Share] CAES-258: removed an useless annotation

--- src/Migrations/2019/02/Version20190211144134.php
+++ src/Migrations/2019/02/Version20190211144134.php
@@ -5,9 +5,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20190211144134 extends AbstractMigration
 {
     public function up(Schema $schema) : void
