7d70d1a4754ca0584eeca50d888d206f5d3054b6
mg-4xxi <mg@4xxi.com>
2019-02-26 12:22:43 +0300

    [Share] CAES-266: fixed flow statuses handling for read only users

--- src/Factory/View/SecurityBootstrapViewFactory.php
+++ src/Factory/View/SecurityBootstrapViewFactory.php
@@ -81,7 +81,11 @@ class SecurityBootstrapViewFactory
         switch (true) {
             case $user->hasRole(User::ROLE_READ_ONLY_USER):
             case $user->hasRole(User::ROLE_ANONYMOUS_USER):
-                $state = User::FLOW_STATUS_INCOMPLETE === $user->getFlowStatus() ? SecurityBootstrapView::STATE_CHECK_SHARED : SecurityBootstrapView::STATE_CHECK;
+                $flowStatuses = [
+                    User::FLOW_STATUS_CHANGE_PASSWORD,
+                    User::FLOW_STATUS_INCOMPLETE,
+                ];
+                $state = in_array($user->getFlowStatus(), $flowStatuses) ? SecurityBootstrapView::STATE_CHECK_SHARED : SecurityBootstrapView::STATE_CHECK;
             break;
             default:
                 $state = is_null($user->getEncryptedPrivateKey()) ? SecurityBootstrapView::STATE_CREATE : SecurityBootstrapView::STATE_CHECK;
