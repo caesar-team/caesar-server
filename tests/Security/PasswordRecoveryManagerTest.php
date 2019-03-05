9a4f712542c0610543011086073aae77fcf432af
mg-4xxi <mg@4xxi.com>
2019-03-05 13:49:08 +0300

    [Password Recovery] CAES-308: init tests

---
+++ tests/Security/PasswordRecoveryManagerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Tests\Security;
+
+use PHPUnit\Framework\TestCase;
+
+class PasswordRecoveryManagerTest extends TestCase
+{
+    public function testResetPassword()
+    {
+
+    }
+
+    public function testGenerateConfirmationToken()
+    {
+
+    }
+
+    public function testSendResettingEmailMessage()
+    {
+
+    }
+
+    public function testConfirmRecovery()
+    {
+
+    }
+}
