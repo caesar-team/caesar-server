fa425e678aed5d25acfe769bc58c30e206cf3488
mg-4xxi <mg@4xxi.com>
2019-03-19 13:33:20 +0300

    [Shares] CAES-336: renamed invites

--- src/Form/Request/Invite/CreateChildItemType.php
+++ src/Form/Request/Invite/CreateChildItemType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class InviteType extends SecretType
+class CreateChildItemType extends SecretType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
