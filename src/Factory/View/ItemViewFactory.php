0c6b499eb2dcdc39b36c750877ca9ea5f1365fd8
mg-4xxi <mg@4xxi.com>
2019-03-19 17:03:07 +0300

    [Shares] CAES-336: fix

--- src/Factory/View/ItemViewFactory.php
+++ src/Factory/View/ItemViewFactory.php
@@ -47,7 +47,6 @@ class ItemViewFactory
         $view->owner = $this->getOwner($item);
         $view->favorite = $item->isFavorite();
         $view->sort = $item->getSort();
-        $view->originalItemId = $item->getOriginalItem()?$item->getOriginalItem()->getId()->toString():null;
 
         return $view;
     }
