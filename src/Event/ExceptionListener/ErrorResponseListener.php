8b174ca6a0c69d047d9f76f5bfd739809dc11741
mg-4xxi <mg@4xxi.com>
2019-04-15 16:03:04 +0300

    [Errors] CAES-374: error standart

---
+++ src/Event/ExceptionListener/ErrorResponseListener.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Event\ExceptionListener;
+
+use App\Exception\ApiException;
+use App\Utils\ErrorMessageFormatter;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class ErrorResponseListener
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        if ('dev' === getenv('APP_ENV')) {
+            $this->logError($exception);
+        }
+
+        $event->setException($this->createException($exception));
+        /** @var ApiException $newException */
+        $newException = $event->getException();
+
+        $event->allowCustomResponseCode();
+        $response = new JsonResponse($newException->getData(), $newException->getCode());
+        $event->setResponse($response);
+    }
+
+    private function logError(\Exception $exception)
+    {
+        $context = [
+            'code' => $exception->getCode(),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+        ];
+        $this->logger->error($exception->getMessage(), $context);
+    }
+
+    private function createException(\Exception $exception): ApiException
+    {
+        if ($exception instanceof ApiException) {
+            return $exception;
+        }
+
+        $data = ErrorMessageFormatter::errorFormat($exception);
+
+        return new ApiException($data, Response::HTTP_BAD_REQUEST);
+    }
+}
