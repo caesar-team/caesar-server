6857f554ad8010141317cdb5900ef6442f21a19e
mg-4xxi <mg@4xxi.com>
2019-04-23 16:57:59 +0300

    [Redis][Message]: restore secure messages

--- src/Controller/Api/MessageController.php
+++ src/Controller/Api/MessageController.php
@@ -8,12 +8,48 @@ use App\Form\Request\SecureMessageType;
 use App\Model\DTO\SecureMessage;
 use App\Services\SecureMessageManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Component\Routing\Annotation\Route;
+use Swagger\Annotations as SWG;
 
 class MessageController extends AbstractController
 {
-
+    /**
+     * @SWG\Tag(name="Secure Message")
+     *
+     * @SWG\Parameter(
+     *     name="body",
+     *     in="body",
+     *     @Model(type="\App\Form\Request\SecureMessageType")
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="Success message created",
+     *     @SWG\Schema(
+     *         type="object",
+     *         @SWG\Property(
+     *             type="string",
+     *             property="id",
+     *             example="fc2b052450a6c890ffa510c2aa735c0178c71a03"
+     *         )
+     *     )
+     * )
+     *
+     * @Route(
+     *     "/api/message",
+     *     name="message_create",
+     *     methods={"POST"}
+     * )
+     *
+     * @param Request              $request
+     * @param SecureMessageManager $messageManager
+     *
+     * @return array|FormInterface
+     */
     public function createMessage(Request $request, SecureMessageManager $messageManager)
     {
         $message = new SecureMessage();
@@ -31,12 +67,39 @@ class MessageController extends AbstractController
         ];
     }
 
+    /**
+     * @SWG\Tag(name="Secure Message")
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Get message by id",
+     *     @SWG\Schema(
+     *         @Model(type="\App\Model\DTO\SecureMessage")
+     *     )
+     * )
+     *
+     * @SWG\Response(
+     *     response=404,
+     *     description="Not found message"
+     * )
+     *
+     * @Route(
+     *     "/api/message/{id}",
+     *     name="get_message_by_id",
+     *     methods={"GET"}
+     * )
+     *
+     * @param string               $id
+     * @param SecureMessageManager $messageManager
+     *
+     * @return SecureMessage
+     */
     public function showMessage(string $id, SecureMessageManager $messageManager)
     {
         $message = $messageManager->get($id);
 
         if (empty($message)) {
-            throw new NotFoundHttpException('No such message');
+            throw new NotFoundHttpException('No such message', Response::HTTP_NOT_FOUND);
         }
 
         return $message;
---
+++ tests/Service/SecureMessageManagerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Service;
+
+use App\Services\SecureMessageManager;
+use PHPUnit\Framework\TestCase;
+use App\Model\DTO\SecureMessage;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class SecureMessageManagerTest extends TestCase
+{
+    /**
+     * @var SecureMessageManager
+     */
+    private $secureMessageManager;
+
+    /**
+     * @throws \ReflectionException
+     */
+    protected function setUp(): void
+    {
+        /** @var \Redis $redis */
+        $redis = $this->createMock(\Redis::class);
+        /** @var SerializerInterface $serializer */
+        $serializer = $this->createMock(SerializerInterface::class);
+        $this->secureMessageManager = new SecureMessageManager($redis, $serializer);
+    }
+
+    public function testSave(SecureMessage $message)
+    {
+        $this->secureMessageManager->save($message);
+
+        $this->assertInstanceOf(SecureMessage::class, $this->secureMessageManager->get($message->getId()));
+    }
+}
