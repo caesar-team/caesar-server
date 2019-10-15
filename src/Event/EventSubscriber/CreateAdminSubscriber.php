3c9594b4a5bde2871f039d059d1d03fdc00ec1ed
mg-4xxi <mg@4xxi.com>
2019-10-15 12:26:29 +0300

    [User] CAES-684: handle user create with admin role

---
+++ src/Event/EventSubscriber/CreateAdminSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Event\EventSubscriber;
+
+
+use App\Entity\User;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+
+class CreateAdminSubscriber implements EventSubscriber
+{
+    /**
+     * Returns an array of events this subscriber wants to listen to.
+     *
+     * @return string[]
+     */
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::prePersist,
+        ];
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $object = $args->getObject();
+
+        if (!$object instanceof User) {
+            return;
+        }
+
+        if (getenv('CHOOSEN_ADMIN_EMAIL') === $object->getEmail()) {
+            $object->addRole(User::ROLE_SUPER_ADMIN);
+        }
+    }
+}
