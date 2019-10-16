b0c55ad8eefba6f2b558d41d288d9ba88f1d481e
mg-4xxi <mg@4xxi.com>
2019-10-16 13:49:14 +0300

    a domain admin join to a default team

--- src/Event/EventSubscriber/CreateAdminSubscriber.php
+++ src/Event/EventSubscriber/CreateAdminSubscriber.php
@@ -6,13 +6,26 @@ declare(strict_types=1);
 namespace App\Event\EventSubscriber;
 
 
+use App\Entity\Team;
 use App\Entity\User;
+use App\Entity\UserTeam;
+use App\Services\TeamManager;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Doctrine\ORM\Events;
 
 class CreateAdminSubscriber implements EventSubscriber
 {
+    /**
+     * @var TeamManager
+     */
+    private $teamManager;
+
+    public function __construct(TeamManager $teamManager)
+    {
+        $this->teamManager = $teamManager;
+    }
+
     /**
      * Returns an array of events this subscriber wants to listen to.
      *
@@ -25,6 +38,10 @@ class CreateAdminSubscriber implements EventSubscriber
         ];
     }
 
+    /**
+     * @param LifecycleEventArgs $args
+     * @throws \Exception
+     */
     public function prePersist(LifecycleEventArgs $args)
     {
         $object = $args->getObject();
@@ -35,6 +52,7 @@ class CreateAdminSubscriber implements EventSubscriber
 
         if (getenv('DOMAIN_ADMIN_EMAIL') === $object->getEmail()) {
             $object->addRole(User::ROLE_SUPER_ADMIN);
+            $this->teamManager->addTeamToUser($object, UserTeam::USER_ROLE_ADMIN);
         }
     }
 }
