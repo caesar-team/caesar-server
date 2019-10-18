c63a7a83c15370160a2729c66e36d7de20492b19
mg-4xxi <mg@4xxi.com>
2019-10-18 18:58:35 +0300

    [Admin] on team create add all user admins to team

--- src/Services/AdminPromoter.php
+++ src/Services/AdminPromoter.php
@@ -35,6 +35,8 @@ final class AdminPromoter
     public function addTeamToAdmins(Team $team, ?User $excludedUser = null): void
     {
         $users = $this->userRepository->findAdmins();
+        $users = array_merge($users, $this->userRepository->findAdmins(User::ROLE_SUPER_ADMIN));
+        $users = array_unique($users);
         foreach ($users as $user) {
             if ($excludedUser === $user) {
                 continue;
