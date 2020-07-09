777e4f0bee8cefd9bef9d1fe0d012494db3751ff
Aleksandr Burov <aburov@4xxi.com>
2020-07-09 11:57:49 +0300

    [Doc] CAES-1005: Refactoring docker-compose

--- tests/api/Team/PermissionTest.php
+++ tests/api/Team/PermissionTest.php
@@ -71,6 +71,9 @@ class PermissionTest extends Unit
         $I->addUserToTeam($team, $user);
         $I->addUserToTeam($team, $teamAdmin, UserTeam::USER_ROLE_ADMIN);
 
+        $adminTeam = $I->createTeam($admin);
+        $I->addUserToTeam($adminTeam, $superAdmin);
+
         $I->login($admin);
         $I->sendGET(sprintf('/teams/%s', $team->getId()->toString()));
         $I->seeResponseContainsJson(['_links' => self::DOMAIN_ADMIN_ACCESS]);
@@ -110,6 +113,8 @@ class PermissionTest extends Unit
 
         $I->login($superAdmin);
         $I->sendGET('/teams');
+        $I->seeResponseContains($adminTeam->getId()->toString());
+        $I->dontSeeResponseContains($team->getId()->toString());
         $I->dontSeeResponseContainsJson(['_links' => self::DOMAIN_ADMIN_ACCESS]);
         $I->dontSeeResponseByJsonPathContainsJson('$[0].users[0]', [
             '_links' => self::USER_TEAM_ACCESS,
