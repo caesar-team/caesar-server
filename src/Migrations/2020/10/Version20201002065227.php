7951b8fccaac02190d90ffc2a9dd25bfac33558d
Aleksandr Burov <aburov@4xxi.com>
2020-10-02 11:32:52 +0300

    [Team] CAES-1323: Added pinned toggle to team

---
+++ src/Migrations/2020/10/Version20201002065227.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20201002065227 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE groups ADD pinned TEXT DEFAULT NULL');
+        $this->addSql('COMMENT ON COLUMN groups.pinned IS \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE groups DROP pinned');
+    }
+}
