41c99f939d86fc8b0afb7e3e8a471fd9291dd94d
Aleksandr Burov <aburov@4xxi.com>
2020-10-13 15:17:50 +0300

    [User] CAES-1374: Added hateoas to user team view

--- src/Model/View/Team/UserTeamView.php
+++ src/Model/View/Team/UserTeamView.php
@@ -6,9 +6,122 @@ namespace App\Model\View\Team;
 
 use App\Entity\Team;
 use App\Entity\UserTeam;
+use Hateoas\Configuration\Annotation as Hateoas;
 use JMS\Serializer\Annotation as Serializer;
 use Swagger\Annotations as SWG;
 
+/**
+ * @Hateoas\Relation(
+ *     "team_delete",
+ *     attributes={"method": "DELETE"},
+ *     href=@Hateoas\Route(
+ *         "api_team_delete",
+ *         parameters={ "team": "expr(object.getId())" }
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\TeamVoter::DELETE'), object.getTeam()))"
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *     "team_edit",
+ *     attributes={"method": "PATCH"},
+ *     href=@Hateoas\Route(
+ *         "api_team_edit",
+ *         parameters={ "team": "expr(object.getId())" }
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\TeamVoter::EDIT'), object.getTeam()))"
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *     "team_create_list",
+ *     attributes={"method": "POST"},
+ *     href=@Hateoas\Route(
+ *         "api_team_create_list",
+ *         parameters={ "team": "expr(object.getId())" }
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\TeamListVoter::CREATE'), object.getTeam()))"
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *     "team_get_lists",
+ *     attributes={"method": "GET"},
+ *     href=@Hateoas\Route(
+ *         "api_team_get_lists",
+ *         parameters={ "team": "expr(object.getId())" }
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\TeamListVoter::SHOW'), object.getTeam()))"
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *     "team_members",
+ *     attributes={"method": "GET"},
+ *     href=@Hateoas\Route(
+ *         "api_team_members",
+ *         parameters={ "team": "expr(object.getId())" }
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\UserTeamVoter::VIEW'), object.getUserTeam()))"
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *     "team_member_add",
+ *     attributes={"method": "POST"},
+ *     href=@Hateoas\Route(
+ *         "api_team_member_add",
+ *         parameters={ "team": "expr(object.getId())", "user": "__USER__" }
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\UserTeamVoter::EDIT'), object.getUserTeam()))"
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *     "team_member_batch_add",
+ *     attributes={"method": "POST"},
+ *     href=@Hateoas\Route(
+ *         "api_team_member_batch",
+ *         parameters={ "team": "expr(object.getId())"}
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\UserTeamVoter::EDIT'), object.getUserTeam()))"
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *     "team_pinned",
+ *     attributes={"method": "POST"},
+ *     href=@Hateoas\Route(
+ *         "api_pinned_team_toggle",
+ *         parameters={ "team": "expr(object.getId())" }
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\TeamVoter::PINNED'), object.getTeam()))"
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *     "team_unpinned",
+ *     attributes={"method": "POST"},
+ *     href=@Hateoas\Route(
+ *         "api_unpinned_team_toggle",
+ *         parameters={ "team": "expr(object.getId())" }
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\TeamVoter::PINNED'), object.getTeam()))"
+ *     )
+ * )
+ * @Hateoas\Relation(
+ *     "team_leave",
+ *     attributes={"method": "POST"},
+ *     href=@Hateoas\Route(
+ *         "api_team_member_leave",
+ *         parameters={ "team": "expr(object.getId())" }
+ *     ),
+ *     exclusion=@Hateoas\Exclusion(
+ *         excludeIf="expr(not is_granted(constant('App\\Security\\Voter\\TeamVoter::LEAVE'), object.getTeam()))"
+ *     )
+ * )
+ */
 final class UserTeamView
 {
     /**
@@ -51,9 +164,15 @@ final class UserTeamView
      */
     private Team $team;
 
-    public function __construct(Team $team)
+    /**
+     * @Serializer\Exclude
+     */
+    private ?UserTeam $userTeam;
+
+    public function __construct(Team $team, ?UserTeam $currentUserTeam = null)
     {
         $this->team = $team;
+        $this->userTeam = $currentUserTeam;
     }
 
     public function getId(): string
@@ -129,5 +248,10 @@ final class UserTeamView
     public function getTeam(): Team
     {
         return $this->team;
+    }
+
+    public function getUserTeam(): ?UserTeam
+    {
+        return $this->userTeam;
     }
 }
