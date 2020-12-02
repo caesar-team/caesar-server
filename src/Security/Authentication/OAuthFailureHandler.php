e1c6227deeb2161ce70be415087090c5fa62ee1d
Aleksandr Burov <aburov@4xxi.com>
2020-12-02 13:09:06 +0300

    [OAuth] CAES-1577: Fixed oauth allowed domains.

--- src/Security/Authentication/OAuthFailureHandler.php
+++ src/Security/Authentication/OAuthFailureHandler.php
@@ -4,21 +4,43 @@ namespace App\Security\Authentication;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 
 class OAuthFailureHandler implements AuthenticationFailureHandlerInterface
 {
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        $url = $request->getSession()->get('current_frontend_uri', '/');
-        if (empty($url)) {
-            $url = '/';
+        $session = $request->getSession();
+
+        $url = $session->get('current_frontend_uri', null);
+        if (null !== $url) {
+            return new RedirectResponse(sprintf('%s?error=%s', $url, $exception->getMessage()));
         }
 
-        return new RedirectResponse($url);
+        $adminTarget = $session->get('_security.admin.target_path');
+        if (null !== $adminTarget) {
+            if ($session instanceof Session) {
+                $session->getFlashBag()->add('danger', $exception->getMessage());
+            }
+
+            return new RedirectResponse(
+                $this->router->generate('_login')
+            );
+        }
+
+        return new RedirectResponse('/');
     }
 }
